b38bdfe6465fe99e13e9ddc3b541079e6930748f
AdamFASHOO <AdamChapman1994@gmail.com>
2022-05-09 15:00:51 -0400

    Lab Finish

---
+++ StudentDatabaseEFCore/Program.cs
@@ -0,0 +1,22 @@
+﻿using StudentDatabaseEFCore;
+using StudentDatabaseEFCore.Models;
+
+StudentContext db = new StudentContext();
+
+List<Student> students = db.Students.ToList();
+
+StudentCRUD sc = new StudentCRUD();
+
+//Student Paul = new Student() { Name = "Paul", Hometown = "South Lyon", FavoriteColor = "Orange", FavoriteFood = "Panda Express" };
+//sc.AddStudent(Paul);
+
+//Student x = new Student() { Name = "x", Hometown = "y", FavoriteFood = "rocks", FavoriteColor = "blind" };
+//sc.AddStudent(x);
+
+Console.WriteLine("Name/Hometown/FavFood/FavColor\n");
+foreach (Student s in sc.GetStudents())
+{
+    Console.WriteLine($"{s.Name}/{s.Hometown}/{s.FavoriteFood}/{s.FavoriteColor}");
+}
+
+//sc.DeleteStudent(5);
---
+++ StudentDatabaseEFCore/StudentCRUD.cs
@@ -0,0 +1,50 @@
+﻿using StudentDatabaseEFCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabaseEFCore
+{
+    class StudentCRUD
+    {
+        StudentContext db = new StudentContext();
+
+        public List<Student> GetStudents()
+        {
+            return db.Students.ToList();
+        }
+
+        public Student GetStudent(int id)
+        {
+            Student output = db.Students.Find(id);
+            if (output != null)
+            {
+                return output;
+            }
+            else
+            {
+                Student badNoodle = new Student();
+                badNoodle.Name = $"No student was found at ID {id}";
+                return badNoodle;
+            }
+        }
+
+        public void AddStudent(Student s)
+        {
+            db.Students.Add(s);
+            db.SaveChanges();
+        }
+
+        public void DeleteStudent(int id)
+        {
+            Student studentToRemove = GetStudent(id);
+            if (db.Students.Contains(studentToRemove))
+            {
+                db.Students.Remove(studentToRemove);
+                db.SaveChanges();
+            }
+        }
+    }
+}
